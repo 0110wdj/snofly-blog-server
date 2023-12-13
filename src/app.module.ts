4a3d515281abd8ee68ae30fec213ecabe524e86d
Snofly <49230243+0110wdj@users.noreply.github.com>
2023-12-13 20:30:00 +0800

    添加数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { TalkController } from './talk/talk.controller';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '47.97.71.176',
+      port: 3306,
+      username: 'snofly',
+      password: 'snofly0110',
+      database: 'blog',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [TalkController],
 })
 export class AppModule { }
